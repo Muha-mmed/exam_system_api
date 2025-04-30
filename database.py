f926838f4e70f3aa8e137160806bbd37ab99a500
Muha-mmed <mrobiu001@gmail.com>
2025-04-30 04:48:42 -0700

    ...

---
+++ database.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+
+Base = declarative_base()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL,connect_args={"check_same_thread": False},echo=True)
+
+localSession = sessionmaker(autoflush=False,autocommit=False,bind= engine)
