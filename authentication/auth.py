69c165d5edfdba903bcf420b003d95cc3c8a0b68
Muha-mmed <mrobiu001@gmail.com>
2025-05-06 09:04:00 -0700

    adjusting

--- authentication/auth.py
+++ authentication/auth.py
@@ -1,11 +1,13 @@
-
-
 from datetime import datetime,timedelta
+from fastapi import Depends, HTTPException
 from jose import JWTError,jwt
 from fastapi.security import OAuth2PasswordBearer
+import os 
 
 oauthSchema = OAuth2PasswordBearer(tokenUrl="/token")
 
+ALGORITHM = os.getenv("ALGORITHM")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
@@ -15,6 +17,20 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
         "iat": datetime.utcnow(),
         "sub": str(data.get("sub"))  # Must include 'sub' in data
     })
-    encoded_jwt = jwt.encode(to_encode, "SECRET_KEY", algorithm="HS256")
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
     return encoded_jwt
 
+def current_user(token: str = Depends(oauthSchema)):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail= "Could not validate credentials",
+        headers= {"WWW-Authenticate": "Bearer"}
+    )
+    try:
+      payload = jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+      email = payload.get({"sub": str })
+      if not email:
+          raise credentials_exception
+    except JWTError:
+      raise credentials_exception
+    return email
